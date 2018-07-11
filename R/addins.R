309c502f94782a5c438bb773c90d6f884d1d6579
Lorenz Walthert <lorenz.walthert@icloud.com>
2018-07-11 13:51:32 +0200

    Add Addin

---
+++ R/addins.R
@@ -0,0 +1,30 @@
+#' Source a custom file - devtools::load_all() if the directory is a Package
+#' 
+#' Checks if project is a package and if so, executes `devtools::load_all()`. 
+#' Otherwise, the `load_file` is sourced.
+#' @param load_file The source file
+#' @param ... Parameters passed to [devtools::load_all()].
+#' @export
+load_all <- function(load_file = getOption("teamtools.load_file"), ...) {
+  if (!file.exists(load_file)) abort()
+    
+  if (
+    !file.exists(here::here("DESCRIPTION")) || 
+    desc::desc_get_field("Type") != "Package"
+  ) {
+    rstudioapi::documentSaveAll()
+    cli::cat_bullet("Sourcing ", load_file, bullet = "tick", col = "green")
+    source(load_file)
+  } else {
+    cli::cat_bullet("Executing devtools::load_all(...)", bullet = "tick", col = "green")
+    if ("devtools" %in% installed.packages()[, "Package"]) {
+      rstudioapi::documentSaveAll()
+      devtools::load_all(...)
+    } else {
+      rlang::abort(paste0(
+        "R package devtools not installed.", 
+        "Please install it to use this functionality."
+     ))
+    }
+  }
+}
