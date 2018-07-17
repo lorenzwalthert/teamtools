9501dc67146d6cb27955c0c0e6a9bf2b2f888bdd
Lorenz Walthert <lorenz.walthert@icloud.com>
2018-07-17 20:49:05 +0200

    make case work when there is no Type: field

--- R/addins.R
+++ R/addins.R
@@ -8,14 +8,9 @@
 load_all <- function(load_file = getOption("teamtools.load_file"), ...) {
     
   if (
-    !file.exists(here::here("DESCRIPTION")) || 
-    desc::desc_get_field("Type") != "Package"
+    file.exists(here::here("DESCRIPTION")) && 
+    (!desc::desc_has_fields("Type") || desc::desc_get_field("Type") == "Package")
   ) {
-    if (!file.exists(load_file)) rlang::abort("load file does not exist.")
-    rstudioapi::documentSaveAll()
-    cli::cat_bullet("Sourcing ", load_file, bullet = "tick", col = "green")
-    source(load_file)
-  } else {
     cli::cat_bullet("Executing devtools::load_all(...)", bullet = "tick", col = "green")
     if ("devtools" %in% installed.packages()[, "Package"]) {
       rstudioapi::documentSaveAll()
@@ -24,7 +19,12 @@ load_all <- function(load_file = getOption("teamtools.load_file"), ...) {
       rlang::abort(paste0(
         "R package devtools not installed.", 
         "Please install it to use this functionality."
-     ))
+      ))
     }
+  } else {
+    if (!file.exists(load_file)) rlang::abort("load file does not exist.")
+    rstudioapi::documentSaveAll()
+    cli::cat_bullet("Sourcing ", load_file, bullet = "tick", col = "green")
+    source(load_file)
   }
 }
