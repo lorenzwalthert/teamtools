98d1a3627672d64e05ee7b3e7d9e4548bb40b9c4
Lorenz Walthert <lorenz.walthert@icloud.com>
2018-09-04 11:01:17 +0200

    only use rstudioapi if available

--- R/addins.R
+++ R/addins.R
@@ -12,7 +12,9 @@ load_all <- function(load_file = getOption("teamtools.load_file"), ...) {
   if (is_package) {
     cli::cat_bullet("Executing devtools::load_all(...)", bullet = "tick", col = "green")
     if ("devtools" %in% installed.packages()[, "Package"]) {
-      rstudioapi::documentSaveAll()
+      if (rstudioapi::isAvailable()) {
+        rstudioapi::documentSaveAll()
+      }
       devtools::load_all(...)
     } else {
       rlang::abort(paste0(
@@ -28,7 +30,9 @@ load_all <- function(load_file = getOption("teamtools.load_file"), ...) {
     }
     if (!file.exists(load_file)) rlang::abort("load file does not exist.")
     
-    rstudioapi::documentSaveAll()
+    if (rstudioapi::isAvailable()) {
+      rstudioapi::documentSaveAll()
+    }
     cli::cat_bullet("Sourcing ", load_file, bullet = "tick", col = "green")
     source(load_file)
   }
