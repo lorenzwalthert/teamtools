21f973b523a32f44824f3f789976978ec5579da0
pat-s <patrick.schratz@gmail.com>
2019-12-03 14:45:32 +0100

    improve doc

--- R/open.R
+++ R/open.R
@@ -4,7 +4,8 @@
 #' regex (i.e. substring matches) are allowed.
 #' @param n For partial matches, the ID of repos to open in parallel. Can be a
 #'   single integer, a sequence (`3:7`) or a vector (`c(2, 5)`). For `n = 1`,
-#'   the exact match is opened.
+#'   the exact match is opened (if there is one) otherwise the best regex fit is
+#'   opened.
 #' @param project A character vector of length one with a project name.
 #'   Can be left `NULL` if the repo name is unique across all projects.
 #' @param dir Any directory under the team root.
@@ -18,28 +19,28 @@ open_repo <- function(repo = "meta",
                       project = NULL,
                       dir = ".",
                       newSession = TRUE) {
-  
-  
+
+
   repos <- git_dirs(dir)
   is_repo_match <- grepl(repo, basename(repos), fixed = TRUE)
   matching_repo <- repos[is_repo_match]
-  
+
   if (all(!is_repo_match)) stop("Make sure the repo exists.", call. = FALSE)
   if (!is.null(project)) {
     is_project_match <- basename(dirname(repos)) == project
     matching_repo <- repos[is_repo_match & is_project_match]
   }
-  
+
   if (!is.null(n) && sum(is_repo_match) > n) {
     cli::cli_text("Argument {.code repo = {repo}} results in multiple partial matches:")
     cli::cat_bullet(matching_repo)
-    
+
     # check if we have exact match
     exact_match <- which(basename(repos) == repo)
     if (n == 1 && length(exact_match == 1)) {
       cli::cli_text("We've found one exact match. Opening project {.code {repo}}.")
       rstudioapi::openProject(matching_repo[which(basename(matching_repo) == repo)],
-                              newSession = newSession
+        newSession = newSession
       )
       return(invisible(TRUE))
     } else {
